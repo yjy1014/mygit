89a0d9592802013b522e03dc14c15a9ac1877cc4
yjy1014 <jiyeong3@daum.net>
2021-09-24 12:20:25 +0900

    Dog.say, Cat.say 호출문 추가

--- 01_hello_git/src/com/kh/git/HelloWorld.java
+++ 01_hello_git/src/com/kh/git/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.kh.git;
 
+import com.kh.animal.Cat;
+import com.kh.animal.Dog;
 import com.kh.foo.Foo;
 
 public class HelloWorld {
@@ -12,6 +14,9 @@ public class HelloWorld {
 		new Foo().sayFoo();
 		new Foo().sayFoo(); // #sayFoo 에서 추가
 		System.out.println("Luck you~"); // #master에서 추가
+		
+		new Dog().say();
+		new Cat().say();
 	}
 
 }
