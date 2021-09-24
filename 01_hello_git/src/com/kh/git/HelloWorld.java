079504c05d03cf0b60cc65f3199fda8753828155
yjy1014 <jiyeong3@daum.net>
2021-09-24 10:45:12 +0900

    sayFoo 기능추가!

--- 01_hello_git/src/com/kh/git/HelloWorld.java
+++ 01_hello_git/src/com/kh/git/HelloWorld.java
@@ -1,10 +1,14 @@
 package com.kh.git;
 
+import com.kh.foo.Foo;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world~");
 		System.out.println("Hello git~");
+		
+		new Foo().sayFoo();
 	}
 
 }
