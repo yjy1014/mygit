41c26022535f358506183d7226d63b21165b72e4
yjy1014 <91200138+yjy1014@users.noreply.github.com>
2021-09-24 14:33:56 +0900

    world 메소드 추가(pull)

--- 01_hello_git/src/com/kh/git/HelloWorld.java
+++ 01_hello_git/src/com/kh/git/HelloWorld.java
@@ -26,10 +26,17 @@ public class HelloWorld {
 	}
 	
 	/**
-	* 원격 저장소에서 수정된 내용
+	* 원격 저장소에서 수정된 내용 #1
 	*/
 	public void hello(){
 		System.out.println("저를 fetch해주세요");
+	}
+	
+	/**
+	* 원격 저장소에서 수정된 내용 #2
+	*/
+	public void world(){
+		System.out.println("저를 pull해주세요");
 	}
 
 }
